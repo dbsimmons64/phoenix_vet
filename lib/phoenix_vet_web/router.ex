fd4e7f1f091ae94656d8da4732433ed7c9d9d7b4
David Simmons <dbsimmons64@outlook.com>
2024-07-13 09:04:47 +0100

    Removed phx.gen.auth (to shortly replace with Ash Auth)

--- lib/phoenix_vet_web/router.ex
+++ lib/phoenix_vet_web/router.ex
@@ -1,8 +1,6 @@
 defmodule PhoenixVetWeb.Router do
   use PhoenixVetWeb, :router
 
-  import PhoenixVetWeb.UserAuth
-
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -10,7 +8,6 @@ defmodule PhoenixVetWeb.Router do
     plug :put_root_layout, html: {PhoenixVetWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug :fetch_current_user
   end
 
   pipeline :api do
@@ -49,40 +46,4 @@ defmodule PhoenixVetWeb.Router do
   end
 
   ## Authentication routes
-
-  scope "/", PhoenixVetWeb do
-    pipe_through [:browser, :redirect_if_user_is_authenticated]
-
-    live_session :redirect_if_user_is_authenticated,
-      on_mount: [{PhoenixVetWeb.UserAuth, :redirect_if_user_is_authenticated}] do
-      live "/users/register", UserRegistrationLive, :new
-      live "/users/log_in", UserLoginLive, :new
-      live "/users/reset_password", UserForgotPasswordLive, :new
-      live "/users/reset_password/:token", UserResetPasswordLive, :edit
-    end
-
-    post "/users/log_in", UserSessionController, :create
-  end
-
-  scope "/", PhoenixVetWeb do
-    pipe_through [:browser, :require_authenticated_user]
-
-    live_session :require_authenticated_user,
-      on_mount: [{PhoenixVetWeb.UserAuth, :ensure_authenticated}] do
-      live "/users/settings", UserSettingsLive, :edit
-      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
-    end
-  end
-
-  scope "/", PhoenixVetWeb do
-    pipe_through [:browser]
-
-    delete "/users/log_out", UserSessionController, :delete
-
-    live_session :current_user,
-      on_mount: [{PhoenixVetWeb.UserAuth, :mount_current_user}] do
-      live "/users/confirm/:token", UserConfirmationLive, :edit
-      live "/users/confirm", UserConfirmationInstructionsLive, :new
-    end
-  end
 end
